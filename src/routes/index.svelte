8f965321752b57d5df46c09e2050a545902a5e4e
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-15 21:23:13 +0700

    Handle keyboard onkeydown

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,15 @@
 <script type="ts">
 	import 'twind/shim';
 	let count: number = 0;
+
+	function incrementCount() {
+		count++;
+	}
 </script>
 
-<svelte:body />
+<svelte:body on:keydown={incrementCount} />
 
-<main class="w-full h-screen flex flex-col items-center justify-center" on:click={() => count++}>
+<main class="w-full h-screen flex flex-col items-center justify-center" on:click={incrementCount}>
 	<h1 class="text-6xl">Popyut</h1>
 	<p class="text-4xl">Count: {count}</p>
 </main>
