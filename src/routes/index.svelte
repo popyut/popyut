994599d94342ea4641531e40fc7f4f8d53497507
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-15 21:21:42 +0700

    Add twind, handle simple click

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+	import 'twind/shim';
+	let count: number = 0;
+</script>
+
+<svelte:body />
+
+<main class="w-full h-screen flex flex-col items-center justify-center" on:click={() => count++}>
+	<h1 class="text-6xl">Popyut</h1>
+	<p class="text-4xl">Count: {count}</p>
+</main>
