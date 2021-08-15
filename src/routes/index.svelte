5fb264762a0e08b8e5ea15c775dc26f9b381bf38
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-15 23:21:20 +0700

    #TeamSpaces

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,156 +1,156 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import axios from 'axios';
-	import 'twind/shim';
+  import { onMount } from 'svelte';
+  import axios from 'axios';
+  import 'twind/shim';
 
-	import { count } from '../lib/store';
+  import { count } from '../lib/store';
 
-	let audio1: HTMLAudioElement;
-	let audio2: HTMLAudioElement;
-	let audio3: HTMLAudioElement;
-	let audio4: HTMLAudioElement;
-	let naja: HTMLAudioElement;
-	let audioIndex = 0;
-	let main: HTMLElement;
-	let bgIndex = 0;
-	let debounceState = false;
-	let total = 0;
-	let lastCount;
+  let audio1: HTMLAudioElement;
+  let audio2: HTMLAudioElement;
+  let audio3: HTMLAudioElement;
+  let audio4: HTMLAudioElement;
+  let naja: HTMLAudioElement;
+  let audioIndex = 0;
+  let main: HTMLElement;
+  let bgIndex = 0;
+  let debounceState = false;
+  let total = 0;
+  let lastCount;
 
-	axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+  axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-	function incrementCount() {
-		if (debounceState) {
-			return;
-		}
-		debounceState = true;
+  function incrementCount() {
+    if (debounceState) {
+      return;
+    }
+    debounceState = true;
 
-		count.update((n) => n + 1);
-		playPop();
-		playNaja();
-		changeBg();
-	}
+    count.update((n) => n + 1);
+    playPop();
+    playNaja();
+    changeBg();
+  }
 
-	function unlockDebounce() {
-		debounceState = false;
-	}
+  function unlockDebounce() {
+    debounceState = false;
+  }
 
-	function playPop() {
-		switch (audioIndex) {
-			case 0:
-				audio1.currentTime = 0;
-				audio1.play();
-				break;
-			case 1:
-				audio2.currentTime = 0;
-				audio2.play();
-				break;
-			case 2:
-				audio3.currentTime = 0;
-				audio3.play();
-				break;
-			case 3:
-				audio4.currentTime = 0;
-				audio4.play();
-				break;
-		}
+  function playPop() {
+    switch (audioIndex) {
+      case 0:
+        audio1.currentTime = 0;
+        audio1.play();
+        break;
+      case 1:
+        audio2.currentTime = 0;
+        audio2.play();
+        break;
+      case 2:
+        audio3.currentTime = 0;
+        audio3.play();
+        break;
+      case 3:
+        audio4.currentTime = 0;
+        audio4.play();
+        break;
+    }
 
-		audioIndex = (audioIndex + 1) % 4;
-	}
+    audioIndex = (audioIndex + 1) % 4;
+  }
 
-	function playNaja() {
-		if ($count % 100 === 0) {
-			naja.currentTime = 0;
-			naja.play();
-		}
-	}
+  function playNaja() {
+    if ($count % 100 === 0) {
+      naja.currentTime = 0;
+      naja.play();
+    }
+  }
 
-	function changeBg() {
-		bgIndex = (bgIndex + 1) % 4;
-	}
+  function changeBg() {
+    bgIndex = (bgIndex + 1) % 4;
+  }
 
-	async function fetchLeaderboard() {
-		try {
-			const res = await axios.get('https://asia-southeast1-popyut.cloudfunctions.net/leaderboard');
+  async function fetchLeaderboard() {
+    try {
+      const res = await axios.get('https://asia-southeast1-popyut.cloudfunctions.net/leaderboard');
 
-			total = res.data.total;
-		} catch (e) {
-			console.error(e);
-		}
-	}
+      total = res.data.total;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-	async function submitCount(count: number) {
-		try {
-			const res = await axios.post('https://asia-southeast1-popyut.cloudfunctions.net/clicks', {
-				data: { n: count }
-			});
+  async function submitCount(count: number) {
+    try {
+      const res = await axios.post('https://asia-southeast1-popyut.cloudfunctions.net/clicks', {
+        data: { n: count },
+      });
 
-			total = res.data.total;
-		} catch (e) {
-			console.error(e);
-		}
-	}
+      total = res.data.total;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-	lastCount = $count;
+  lastCount = $count;
 
-	onMount(fetchLeaderboard);
+  onMount(fetchLeaderboard);
 
-	setInterval(() => {
-		const intervalCount = $count - lastCount;
-		lastCount = $count;
+  setInterval(() => {
+    const intervalCount = $count - lastCount;
+    lastCount = $count;
 
-		submitCount(intervalCount);
-	}, 5000);
+    submitCount(intervalCount);
+  }, 5000);
 </script>
 
 <svelte:body on:keydown={incrementCount} on:keyup={unlockDebounce} />
 <svelte:head><title>Popyut (Beta)</title></svelte:head>
 
 <main
-	bind:this={main}
-	class="w-full h-screen flex flex-col items-center justify-center"
-	on:mousedown={incrementCount}
-	on:mouseup={unlockDebounce}
-	style={`background-image: url('images/p${bgIndex + 1}.jpg');"`}
+  bind:this={main}
+  class="w-full h-screen flex flex-col items-center justify-center"
+  on:mousedown={incrementCount}
+  on:mouseup={unlockDebounce}
+  style={`background-image: url('images/p${bgIndex + 1}.jpg');"`}
 >
-	<h1 class="noselect text-6xl border-black text-white bg-black rounded p-2 flex items-start">
-		Popyut <span class="text-xs text-red-300 mt-2 ml-2">Beta</span>
-	</h1>
-	<p class="noselect text-3xl border-black text-white mt-8 bg-black rounded p-2">Count: {$count}</p>
-	<p class="noselect text-5xl border-black text-white mt-8 bg-black rounded p-2">Total: {total}</p>
+  <h1 class="noselect text-6xl border-black text-white bg-black rounded p-2 flex items-start">
+    Popyut <span class="text-xs text-red-300 mt-2 ml-2">Beta</span>
+  </h1>
+  <p class="noselect text-3xl border-black text-white mt-8 bg-black rounded p-2">Count: {$count}</p>
+  <p class="noselect text-5xl border-black text-white mt-8 bg-black rounded p-2">Total: {total}</p>
 
-	<audio bind:this={audio1}>
-		<source src="pop1.ogg" type="audio/ogg" />
-	</audio>
+  <audio bind:this={audio1}>
+    <source src="pop1.ogg" type="audio/ogg" />
+  </audio>
 
-	<audio bind:this={audio2}>
-		<source src="pop2.ogg" type="audio/ogg" />
-	</audio>
+  <audio bind:this={audio2}>
+    <source src="pop2.ogg" type="audio/ogg" />
+  </audio>
 
-	<audio bind:this={audio3}>
-		<source src="pop3.ogg" type="audio/ogg" />
-	</audio>
+  <audio bind:this={audio3}>
+    <source src="pop3.ogg" type="audio/ogg" />
+  </audio>
 
-	<audio bind:this={audio4}>
-		<source src="pop4.ogg" type="audio/ogg" />
-	</audio>
+  <audio bind:this={audio4}>
+    <source src="pop4.ogg" type="audio/ogg" />
+  </audio>
 
-	<audio bind:this={naja}>
-		<source src="naja.mp3" type="audio/mpeg" />
-	</audio>
+  <audio bind:this={naja}>
+    <source src="naja.mp3" type="audio/mpeg" />
+  </audio>
 </main>
 
 <style>
-	main {
-		background-repeat: no-repeat;
-		background-size: cover;
-	}
+  main {
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-	.noselect {
-		user-select: none; /* supported by Chrome and Opera */
-		-webkit-user-select: none; /* Safari */
-		-khtml-user-select: none; /* Konqueror HTML */
-		-moz-user-select: none; /* Firefox */
-		-ms-user-select: none; /* Internet Explorer/Edge */
-	}
+  .noselect {
+    user-select: none; /* supported by Chrome and Opera */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+  }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,15 @@ import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: vercel(),
+  },
 };
 
 export default config;
