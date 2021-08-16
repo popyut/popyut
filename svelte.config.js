4b56787c87f3e09c6401e62aec71f91f0be04800
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-16 22:03:52 +0700

    Use adapter-static instead of adapter-vercel (#4)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
+// import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +11,12 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: vercel(),
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
   },
 };
 
