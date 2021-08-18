57e6208b91818954202936e1dbfc7657c85cb2ec
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-17 22:21:41 -0700

    max out leaderboard width when top-three element is hidden

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -353,13 +353,11 @@
               {/if}
             </span>
           </div>
-          {#if !showFullLeaderboard}
-          <div class="top-three flex justify-between items-center pt-2">
+          <div class="top-three flex justify-between items-center pt-2 {`${showFullLeaderboard && 'hide'}`}">
             {#each leaderboardGuilds.slice(0, 3) as guild, idx}
               <span>{idx + 1}. {guild.emoji} {guild.name}: {abbreviateNumber(guild.total)}</span>
             {/each}
           </div>
-          {/if}
         </div>
         <div class={`modalBody ${showFullLeaderboard && 'open'}`}>
           {#if showBodyLeader}
@@ -450,6 +448,13 @@
 
   .modalHeader .top-three {
     gap: 0.75rem;
+  }
+
+  .modalHeader .top-three.hide {
+    visibility: hidden;
+    padding: 0;
+    margin: 0;
+    height: 0;
   }
 
   .modalBody {
