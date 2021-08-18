4f7518defce7442830c9a7d5132ae387001319ab
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-18 10:58:34 +0000

    Compact number compact function

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -94,30 +94,8 @@
     }
   }
 
-  /**
-   * Code From
-   * https://stackoverflow.com/questions/10599933/convert-long-number-into-abbreviated-string-in-javascript-with-a-special-shortn
-   */
   function abbreviateNumber(value: number) {
-    let newValue: any = value;
-    if (value >= 1000) {
-      var suffixes = ['', 'k', 'm', 'b', 't'];
-      var suffixNum = Math.floor(('' + value).length / 3);
-      var shortValue: any = '';
-      for (var precision = 2; precision >= 1; precision--) {
-        shortValue = parseFloat(
-          (suffixNum != 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(precision),
-        );
-        var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
-        if (dotLessShortValue.length <= 2) {
-          break;
-        }
-      }
-      if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
-      newValue = shortValue + suffixes[suffixNum];
-    }
-
-    return newValue;
+    return new Intl.NumberFormat('th-TH', {notation: 'compact'}).format(value);
   }
 
   function spin(node: HTMLParagraphElement, t: {}) {
