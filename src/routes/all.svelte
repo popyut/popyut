6cf8aa509b9336f841663f6f46ad6b448c1e5a5b
55555555555555555555555555555555555 <55555555555555555555555555555555555@users.noreply.github.com>
2021-08-18 22:49:54 +0700

    Add /all to pop every web at once

---
+++ src/routes/all.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export const ssr = false;
+  import 'twind/shim';
+</script>
+
+<svelte:head>
+  <script src="https://unpkg.com/@ungap/custom-elements-builtin"></script>
+  <script src="https://unpkg.com/x-frame-bypass"></script>
+</svelte:head>
+
+<main class="w-full h-screen flex flex-row items-center justify-center bg-transparent">
+  <iframe src="https://prayut.click" frameborder="0" class="flex-1 border-0 h-full" />
+  <iframe src="https://popcat.click" frameborder="0" class="flex-1 border-0 h-full" />
+  <iframe
+    src="https://popdog.click/play"
+    frameborder="0"
+    class="flex-1 border-0 h-full"
+    is="x-frame-bypass"
+  />
+  <iframe src="https://popass.click" frameborder="0" class="flex-1 border-0 h-full" />
+</main>
